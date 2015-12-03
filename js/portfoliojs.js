955e2987b2cfe99bb864bf4e8289c2cdaf75a182
Johall <johnalexander.w.hall@gmail.com>
2015-12-02 23:18:02 -0500

    added list of github repos and git commits through ajax

--- js/portfoliojs.js
+++ js/portfoliojs.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
 
 
+//calls repos for list group items
+$.ajax({
+ type: "GET",
+ url: "https://api.github.com/users/WolfgangHall/repos",
+ success: function(repos) {
+  for (var i = 0; i < repos.length; i++) {
+   var newListItem = buildListGroup (repos[i]);
+   $(".list-group").append(newListItem);
+  }
+ },
+ error: function (jqXHR, textStatus) {
+  alert("Something is wrong here");
+ }
+});
+
+function buildListGroup (repoData) {
+ var commitsApiUrl = "https://api.github.com/repos/";
+ commitsApiUrl += repoData.owner.login + "/";
+ commitsApiUrl += repoData.name + "/commits";
+
+ var newLink = $("<a>")
+  .attr("href", commitsApiUrl)
+  .addClass("list-group-item")
+  .append(repoData.full_name);
+  return newLink
+ }
+
+
+//calls commit messages for table
+
 });
