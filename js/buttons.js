b1ad0602a4b89807e20200efd889233997b16e33
Johall <johnalexander.w.hall@gmail.com>
2015-12-07 15:17:23 -0500

    added tech button / added heroku hack / increased size of jumbotron

--- js/buttons.js
+++ js/buttons.js
@@ -6,15 +6,21 @@ $( document ).ready(function() {
     }, 1800);
   });
 
-  $("#projectBtn").click(function(e) {
+  $("#projectBtn").click(function() {
     $('html, body').animate({
       scrollTop: $("#allprojects").offset().top
     }, 1800);
   });
 
-  $("#mainBtn").click(function(e) {
+  $("#mainBtn").click(function() {
     $('html, body').animate({
       scrollTop: $("#allprojects").offset().top
+    }, 1800);
+  });
+
+  $("#techBtn").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#techsection").offset().top
     }, 1800);
   });
 
