3e16b9e8d91ee3b01fa083eade89721ee4b41436
WolfgangHall <johnalexander.w.hall@gmail.com>
2016-04-27 11:42:24 -0400

    button functionality

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -2,27 +2,22 @@ $( document ).ready(function() {
 
   $("#aboutBtn").click(function() {
     $('html, body').animate({
-      scrollTop: $("#aboutsection").offset().top
+      scrollTop: $("#about-section").offset().top
     }, 1800);
   });
 
   $("#projectBtn").click(function() {
     $('html, body').animate({
-      scrollTop: $("#allprojects").offset().top
+      scrollTop: $("#projectsection").offset().top
+    }, 1800);
+  });  
+
+  $("#contactBtn").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#media-section").offset().top
     }, 1800);
   });
 
-  $("#mainBtn").click(function() {
-    $('html, body').animate({
-      scrollTop: $("#allprojects").offset().top
-    }, 1800);
-  });
-
-  $("#techBtn").click(function() {
-    $('html, body').animate({
-      scrollTop: $("#techsection").offset().top
-    }, 1800);
-  });
 
 });
 
